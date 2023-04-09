6ad1d86955f5503534d87210dc96f54123a44bae
sambhavvk <sambhavvk2022@outlook.com>
2023-04-09 12:14:44 +0530

    added test

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'tailwindcss/tailwind.css'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
